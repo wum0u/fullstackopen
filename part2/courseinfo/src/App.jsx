49cece9520a0a5fb44ef0e3b919632bf2f146fec
wumou <16879992+wum0u@users.noreply.github.com>
2025-09-24 15:34:46 +0800

    add: init part2

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+const Course = (props) => {
+
+}
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
